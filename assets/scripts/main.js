70cb021995958cf6df7136c0711702ab6039dcc9
marcantonio99 <marcantonio8799@gamil.com>
2022-10-21 11:44:06 +0200

    ticketprice

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,28 @@
-
+let buttonHtml = document.getElementById('btnInvio');
+let inputName = document.getElementById('inputName');
 let inputKm = document.getElementById('inputKm');
 let inputAge = document.getElementById('inputAge');
+let priceKm = 0.21;
 
-function ticketPrice() { 
+buttonHtml.addEventListener('click', function(){
+    const valueName = inputName.value;
+    const valueKm = inputKm.value;
+    const valueAge = inputAge.value;
+    console.log(valueName, valueKm, valueAge);
 
-    let km = parseInt(inputKm.value);
-    let age = parseInt(inputAge.value);
-    console.log(km, age);
-    let priceTicket = km * 0.21;
-
-    if ( age >= 18 && age < 65 ){
-        document.getElementById('boxPrice').innerHTML = `Il prezzo del biglietto è : ${priceTicket.toFixed(2)}`;
-    } else if ( age < 18 ){
-        document.getElementById('boxPrice').innerHTML = `Il prezzo del biglietto è : ${(priceTicket * 0.80).toFixed(2)}`;
-    } else {
-        document.getElementById('boxPrice').innerHTML = `Il prezzo del biglietto è : ${(priceTicket * 0.60).toFixed(2)}`;
+    if(valueName == '' || valueKm == '' || valueAge == '' ){
+        console.log('compila tutto');
+    }else {
+        let ticketPrice = valueKm * priceKm;
+        let priceMaggiorenne = ticketPrice;
+        let priceMinorenne = ticketPrice * 0.80;
+        let priceOver65 = ticketPrice * 0.60;
+        if( valueAge == 'maggiorenne'){
+            document.getElementById('prezzo').innerHTML = `<p>Il prezzo del biglietto e : ${priceMaggiorenne.toFixed(2)}</p>`
+        }else if(valueAge == 'minorenne'){
+            document.getElementById('prezzo').innerHTML = `<P>Il prezzo del biglietto e : ${priceMinorenne.toFixed(2)}</P>`
+        }else if(valueAge == 'over65'){
+            document.getElementById('prezzo').innerHTML = `<P>Il prezzo del biglietto e : ${priceOver65.toFixed(2)}</P>`
+        }
     }
-
-}
+})
